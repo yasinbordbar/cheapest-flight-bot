2c52d1b8c09a815a2812c84cdc0aa1843d15c792
Yasin Bordbar <yasinbordbar@outlook.com>
2022-07-10 02:19:26 +0430

    Add functionality the cheapest flight of the next week

---
+++ README.md
@@ -0,0 +1,5 @@
+### To Do:
+
+- [ ] Convert to TypeScript
+- [ ] Bypass Captcha 
+
---
+++ driverUtils.js
@@ -0,0 +1,69 @@
+import { Builder, By, until } from "selenium-webdriver";
+import { convertToISODate } from "./utils.js";
+
+export const driver = await new Builder().forBrowser("chrome").build();
+
+const goToWebsite = async () => {
+  const tomorrow = new Date();
+  tomorrow.setDate(new Date().getDate() + 1);
+
+  await driver.get(
+    `https://flightio.com/flight/search/1/THR-BRU/${convertToISODate(
+      tomorrow
+    )}/1-0-0-1`
+  );
+};
+
+const waitToLoadPage = () =>
+  driver.wait(
+    until.elementLocated(
+      By.className(
+        "flabel ResultItemPriceLabel_price__ayC_S   flabel--hasnot-bg"
+      )
+    )
+  );
+
+const findTheFirstPrice = () =>
+  driver
+    .findElement(
+      By.className(
+        "flabel ResultItemPriceLabel_price__ayC_S   flabel--hasnot-bg"
+      )
+    )
+    .getText();
+
+const waitUntilButtonIsClickable = async () => {
+  await driver.wait(
+    until.elementIsEnabled(
+      driver.findElement(
+        By.className(
+          "btn searchresult-header__change-date-btn searchresult-header__nextdate-btn"
+        )
+      )
+    )
+  );
+};
+
+const clickOnNextButton = async () => {
+  await driver
+    .findElement(
+      By.className(
+        "btn searchresult-header__change-date-btn searchresult-header__nextdate-btn"
+      )
+    )
+    .click();
+};
+
+const goToNextPage = async () => {
+  await waitUntilButtonIsClickable();
+  await clickOnNextButton();
+};
+
+export {
+  clickOnNextButton,
+  goToNextPage,
+  findTheFirstPrice,
+  waitUntilButtonIsClickable,
+  waitToLoadPage,
+  goToWebsite,
+};
